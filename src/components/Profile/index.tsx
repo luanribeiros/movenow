5ac66deee0c993087fb551c1483bc7efcd14d138
Luan Ribeiro <luanribeiros@gmail.com>
2021-02-26 11:29:32 -0300

    creating method complete Challenge and your functionality

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,17 +1,21 @@
+import { useContext } from 'react'
+import { ChallengesContext } from '../../contexts/ChallengeContext'
 import styles from './Profile.module.css'
 
 export function Profile() {
-    return (
-      <div className={styles.profileContainer}>
-        <img src="https://github.com/luanribeiros.png" alt="Imagem Perfil Github Luan Ribeiro"/>
-        <div>
-          <strong>Luan Ribeiro</strong>
-          
-          <p>
-            <img src="icons/level.svg" alt="Level"/>
-            Level 1
-          </p>
-        </div>
+  const { level } = useContext(ChallengesContext)
+
+  return (
+    <div className={styles.profileContainer}>
+      <img src="https://github.com/luanribeiros.png" alt="Imagem Perfil Github Luan Ribeiro"/>
+      <div>
+        <strong>Luan Ribeiro</strong>
+        
+        <p>
+          <img src="icons/level.svg" alt="Level"/>
+          Level { level }
+        </p>
       </div>
-    )
-  }
+    </div>
+  )
+}
