13c91c5f7d19c197c42527e79a263a51bf678bd2
Luan Ribeiro <luanribeiros@gmail.com>
2021-02-26 11:57:53 -0300

    creating method CountdownContext and your functionality

--- src/components/Countdown/index.tsx
+++ src/components/Countdown/index.tsx
@@ -1,88 +1,68 @@
 import { useState, useEffect, useContext } from 'react'
-import { ChallengesContext } from '../../contexts/ChallengeContext'
+import { CountdownContext } from '../../contexts/CountdownContext'
 
 import styles from './Countdown.module.css'
 
-let countdownTimeout: NodeJS.Timeout
+
 
 export function Countdown() {
-    const { startNewChallenge } = useContext(ChallengesContext)
-
-    const [time, setTime] = useState(0.05 * 60)
-    const [isActive, setIsActive] = useState(false)
-    const [hasFinished, setHasFinished] = useState(false)
-
-    const minutes = Math.floor(time / 60);
-    const seconds = time % 60;
+  const {
+    minutes, 
+    seconds, 
+    hasFinished, 
+    isActive, 
+    startCountdown, 
+    resetCountdown 
+  } = useContext(CountdownContext)
   
-    const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('')
-    const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('')
-  
-    function startCountdown() {
-      setIsActive(true)
-    }
-    
-    function resetCountdown() {
-      clearTimeout(countdownTimeout)
-      setIsActive(false)
-      setTime(0.05 * 60)
-    }
+  const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('')
+  const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('')
 
-    useEffect(() => {
-      if(isActive && time > 0) {
-        countdownTimeout = setTimeout(() => {
-          setTime(time - 1)
-        }, 1000)
-      } else if (isActive && time === 0) {
-        setHasFinished(true)
-        setIsActive(false)
-        startNewChallenge()
-      }
-    }, [isActive, time])
   
-    return (
-      <div>
-        <div className={styles.countdownContainer}>
-          <div>
-            <span>{minuteLeft}</span>
-            <span>{minuteRight}</span>
-          </div>
-          <span>:</span>
-          <div>
-            <span>{secondLeft}</span>
-            <span>{secondRight}</span>
-          </div>
+
+  return (
+    <div>
+      <div className={styles.countdownContainer}>
+        <div>
+          <span>{minuteLeft}</span>
+          <span>{minuteRight}</span>
         </div>
-
-        { hasFinished ? (
-          <button 
-            disabled
-            className={styles.countdownButton}
-          >
-            Ciclo Encerrado
-          </button>
-        ) : (
-          <>
-            { isActive ? (
-              <button 
-                type="button" 
-                className={`${styles.countdownButton} ${styles.countdownButtonActive}`}
-                onClick={resetCountdown}
-              >
-                Abandonar ciclo
-              </button>
-            ) : (
-              <button 
-                type="button" 
-                className={styles.countdownButton}
-                onClick={startCountdown}
-              >
-                Iniciar um ciclo
-              </button>
-            )}
-          </>
-        )}
-        
+        <span>:</span>
+        <div>
+          <span>{secondLeft}</span>
+          <span>{secondRight}</span>
+        </div>
       </div>
-    )
-  }
+
+      { hasFinished ? (
+        <button 
+          disabled
+          className={styles.countdownButton}
+        >
+          Ciclo Encerrado
+        </button>
+      ) : (
+        <>
+          { isActive ? (
+            <button 
+              type="button" 
+              className={`${styles.countdownButton} ${styles.countdownButtonActive}`}
+              onClick={resetCountdown}
+            >
+              Abandonar ciclo
+            </button>
+          ) : (
+            <button 
+              type="button" 
+              className={styles.countdownButton}
+              onClick={startCountdown}
+            >
+              Iniciar um ciclo
+            </button>
+          )}
+        </>
+      )}
+      
+    </div>
+  )
+}
